08e938f8f584829d35ca6c52d96ff436b35623fe
manas <adikimanas@gmail.com>
2025-09-21 12:49:51 +0530

    commiting pizzacontroller

--- src/main/java/com/niit/PizzaService/controller/PizzaController.java
+++ src/main/java/com/niit/PizzaService/controller/PizzaController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-
+// pizza controller
 @RestController
 @RequestMapping("pizza/")
 public class PizzaController {
